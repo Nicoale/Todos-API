22ed398f434b186c47f4a82f636e6ed8799a1a44
nicoale <nicoale8786@gmail.com>
2020-05-12 05:49:17 +0300

    changed routes to accomodate user devise auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+  namespace :v1, default: {format: :json}do
+      mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
 end
