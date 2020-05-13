f2750d9bbdc3ccca329c8589cfa05e91900cf65a
nicoale <nicoale8786@gmail.com>
2020-05-13 06:45:25 +0300

    routes update solving incorrect routing for the 2 versions of API

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@
 Rails.application.routes.draw do 
 namespace :api do 
   namespace :v0 do 
-    namespace :v1, default: { format: :json } do
-    resources :pings, only[:index], constraints: { format: 'json' } 
+    resources :pings, only: [:index], constraints: { format: 'json' } 
     devise_for :users
-      mount_devise_token_auth_for 'User', at: 'auth', skip: %i[omniauth_callbacks]
-      root to: 'home#index'
-    end
+  end
+    namespace :v1, default: { format: :json } do
+    mount_devise_token_auth_for 'User', at: 'auth', skip: %i[omniauth_callbacks]
   end
 end
+root to: 'home#index'
 end
