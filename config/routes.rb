8cbcd0491a4a8036ab5219473d72fa2462c06755
nicoale <nicoale8786@gmail.com>
2020-05-13 12:13:49 +0300

    fixing issues raised by hound

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
-namespace :api do 
+  namespace :api do 
   namespace :v0 do 
     resources :pings, only[:index], constraints: { format: 'json' } 
     devise_for :users
   end
-    namespace :v1, default: { format: :json } do
+  namespace :v1, default: { format: :json } do
       mount_devise_token_auth_for 'User', at: 'auth', skip: %i[omniauth_callbacks]
-    end
+  end
 end
 root to: 'home#index'
 end
